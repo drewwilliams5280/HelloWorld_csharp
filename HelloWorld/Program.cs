8ed36f7c2b12c5c3d4007a5a5a7eb55093c97915
Drew <dreww@mortgage365.com>
2021-03-12 13:01:15 -0700

    39

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,8 +6,18 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteSomething();
+            WriteSomethingSpecific("I am an argument");
+        }
+        // access mmodifier (static) return type method name (parameter1, parameter2)
+        public static void WriteSomething()
+        {
             Console.WriteLine("Hello Drew!");
+        }
+
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
             Console.Read();
         }
     }
