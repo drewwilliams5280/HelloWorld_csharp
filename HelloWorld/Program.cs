58b4da5b3ca23cc3cfa46af5978170886ee8db69
Drew <dreww@mortgage365.com>
2021-03-12 10:47:06 -0700

    More C# progress. Data types, string interpolation, console logging, etc.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,35 +6,83 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a sting and press Enter:");
-            string readInput = Console.ReadLine();
-            Console.WriteLine("You have entered {0}", readInput);
 
-            Console.Write("Enter a string and press Enter:");
-            int asciiValue = Console.Read();
-            Console.WriteLine("ASCII value is {0}", asciiValue);
-            Console.ReadKey();
-    
-           /* string myname = "Drew";
 
-            string message = $"My name is {myname}";
 
-            string capsMessage = message.ToUpper();
+           /* string myString = "Hello";
+            string mySubString = myString.Substring(2);
+            Console.WriteLine(mySubString);
 
-            Console.WriteLine(capsessage);
+            string myBigString = "Oh my goodness this string is massive, also my name is Drew.";
+            int bigStringGoodness = myBigString.IndexOf("goodness");
+            Console.WriteLine(bigStringGoodness);
+
+            var firstName = "Drew";
+            var lastName = "Williams";
+            string nameString = String.Format("My name is {0} {1}", firstName, lastName);
+            Console.WriteLine(nameString);
+            Console.WriteLine(String.Format("My name is {0} {1}", firstName, lastName));
+
             Console.Read();
+           ***********************************
+            /*  string myString = "15";
+              string mySecondString = "13";
+              int num1 = Int32.Parse(myString);
+              int num2 = Int32.Parse(mySecondString);
+              int result = num1 + num2;
 
-              int num1 = 13;
-                int num2 = 5;
-                int sum = num1 + num2;
+              Console.WriteLine(result);
+              Console.Read();
+              *********************
+              /* double myDouble = 13.97;
+               int myInt;
+               string myDoubleString = myDouble.ToString();
+               bool sunIsShining = false;
+               string myBoolString = sunIsShining.ToString();
 
-                double d1 = 3.5;
-                double d2 = 1.337;
-                double sumD = d1 + d2;
+               myInt = (int)myDouble;
+               Console.WriteLine(myDouble);
+               Console.WriteLine(myInt);
+               Console.WriteLine(myDoubleString);
+               Console.WriteLine(myBoolString);
+               Console.Read();
+              ********************************
+             /*  Console.ForegroundColor = ConsoleColor.Yellow;
+               Console.BackgroundColor = ConsoleColor.DarkGray;
+               Console.Clear();
+               Console.WriteLine("Hello Drew");
+               Console.WriteLine("C# is cool!");
+               Console.Read();
+             ***********************************
+               Console.Write("Enter a sting and press Enter:");
+               string readInput = Console.ReadLine();
+               Console.WriteLine("You have entered {0}", readInput);
 
-                Console.WriteLine($"The sum of {num1} and {num2} is {sum}");
-                Console.WriteLine($"The sum of {d1} and {d2} is {sumD}");
-                Console.Read(); */
+               Console.Write("Enter a string and press Enter:");
+               int asciiValue = Console.Read();
+               Console.WriteLine("ASCII value is {0}", asciiValue);
+               Console.ReadKey();
+               **************************************
+               string myname = "Drew";
+
+               string message = $"My name is {myname}";
+
+               string capsMessage = message.ToUpper();
+
+               Console.WriteLine(capsessage);
+               Console.Read();
+             ************************************************
+                 int num1 = 13;
+                   int num2 = 5;
+                   int sum = num1 + num2;
+
+                   double d1 = 3.5;
+                   double d2 = 1.337;
+                   double sumD = d1 + d2;
+
+                   Console.WriteLine($"The sum of {num1} and {num2} is {sum}");
+                   Console.WriteLine($"The sum of {d1} and {d2} is {sumD}");
+                   Console.Read(); */
         }
     }
 }
