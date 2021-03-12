428cc7c20cf6c2dca59f6cc8b33bfaf33ad47cf9
Drew <dreww@mortgage365.com>
2021-03-12 13:49:12 -0700

    44

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,15 +7,27 @@ namespace HelloWorld
   
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter first number:");
-            string input1 = Console.ReadLine();
-            Console.WriteLine("Enter number to add to first number:");
-            string input2 = Console.ReadLine();
-            int total = Int32.Parse(input1) + Int32.Parse(input2);
-            Console.WriteLine(total);
-            Console.Read();
-        }
-        // access mmodifier (static) return type method name (parameter1, parameter2)
-       
+            Console.WriteLine("Please enter a number:");
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Format Exception, please enter a number");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("General Exception, please enter a number");
+            }
+            finally
+            {
+                Console.WriteLine("This is called anyway.");
+            }
+
+            Console.ReadKey();
+        }       
     }
 }
