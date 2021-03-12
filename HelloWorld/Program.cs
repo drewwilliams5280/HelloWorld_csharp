17083e2e3eb2a5db843a6d34831b0e6863f10f96
Drew <dreww@mortgage365.com>
2021-03-12 13:19:00 -0700

    41, different way

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,14 +9,14 @@ namespace HelloWorld
         const string friend3 = "Jack";
         static void Main(string[] args)
         {
-            GreetFriend(friend1);
-            GreetFriend(friend2);
-            GreetFriend(friend3);
+            GreetFriend(friend1, friend2, friend3);
         }
         // access mmodifier (static) return type method name (parameter1, parameter2)
-        public static void GreetFriend(string friend)
+        public static void GreetFriend(string friend1, string friend2, string friend3)
         {
-            Console.WriteLine(friend);
+            Console.WriteLine(friend1);
+            Console.WriteLine(friend2);
+            Console.WriteLine(friend3);
         }
     }
 }
