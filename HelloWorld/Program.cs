eff68caebf04ed4779ecac4a9c7bb5974dd39d17
Drew <dreww@mortgage365.com>
2021-03-12 11:36:26 -0700

    Fixed

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,21 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            
+            string response;
+            Console.WriteLine("Enter a string here:");
+            response = Console.ReadLine();
+            string search;
+            Console.WriteLine("Enter the character to search:");
+            search = Console.ReadLine();
+            int searchIndex = response.IndexOf(search);
+            Console.WriteLine(searchIndex);
+            string firstName;
+            string lastName;
+            Console.WriteLine("Enter your first name:");
+            firstName = Console.ReadLine();
+            Console.WriteLine("Enter your last name:");
+            lastName = Console.ReadLine();
+            Console.WriteLine(String.Format("{0} {1}", firstName, lastName));
         }
     }
 }
