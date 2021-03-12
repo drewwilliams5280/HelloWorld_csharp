2fc88e5f3010b3aa624668f72864163c154a72bb
Drew <dreww@mortgage365.com>
2021-03-12 14:09:18 -0700

    45

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,27 +7,66 @@ namespace HelloWorld
   
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a number:");
-            string userInput = Console.ReadLine();
+            int num1 = 5;
+            int num2 = 3;
+            int num3;
 
-            try
-            {
-                int userInputAsInt = int.Parse(userInput);
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Format Exception, please enter a number");
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("General Exception, please enter a number");
-            }
-            finally
-            {
-                Console.WriteLine("This is called anyway.");
-            }
+            //unary operator
 
-            Console.ReadKey();
-        }       
+            num3 = -num1;
+            Console.WriteLine("num3 is {0}", num3);
+
+            bool isSunny = true;
+            Console.WriteLine("is it sunny? {0}", !isSunny);
+
+            //increment operators
+            int num = 0;
+            num++;
+            Console.WriteLine("num is {0}", num);
+            //post increment
+            Console.WriteLine("num is {0}", num++);
+            Console.WriteLine("num is {0}", num);
+            // pre increment
+            Console.WriteLine("num is {0}", ++num);
+            Console.WriteLine("num is {0}", ++num);
+
+            // decrement
+            num--;
+            Console.WriteLine("num is {0}", num);
+            //post decrement
+            Console.WriteLine("num is {0}", num--);
+            Console.WriteLine("num is {0}", num);
+            // pre decrement
+            Console.WriteLine("num is {0}", --num);
+            Console.WriteLine("num is {0}", --num);
+
+            int result;
+
+            result = num1 + num2;
+            Console.WriteLine("Result of num1 + num2 is {0}", result);
+            result = num1 - num2;
+            Console.WriteLine("Result of num1 - num2 is {0}", result);
+            result = num1 / num2;
+            Console.WriteLine("Result of num1 / num2 is {0}", result);
+            result = num1 * num2;
+            Console.WriteLine("Result of num1 * num2 is {0}", result);
+            result = num1 % num2;
+            Console.WriteLine("Result of num1 % num2 is {0}", result);
+
+            // relational and type operators
+            bool isLower;
+            isLower = num1 < num2;
+            Console.WriteLine("Result of num1 < num2 is {0}", isLower);
+            isLower = num1 > num2;
+            Console.WriteLine("Result of num1 < num2 is {0}", isLower);
+            bool isEqual = num1 == num2;
+            Console.WriteLine("Result of num1 == num2 is {0}", isEqual);
+
+            //conditional operators
+
+            bool isLowerAndSunny;
+            isLowerAndSunny = isLower && isSunny;
+            Console.WriteLine(isLowerAndSunny);
+        }
     }
 }
