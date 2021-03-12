c8c6a2f4a5d9f783266dc87f6177acdaad843495
Drew <dreww@mortgage365.com>
2021-03-12 11:52:55 -0700

    Completes challenge

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,9 +6,21 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello Drew!");
-            Console.Read();
+            byte myByte = 45;
+            Console.WriteLine(myByte);
+            sbyte mySbyte = -120;
+            Console.WriteLine(mySbyte);
+            int myInt = 465658;
+            Console.WriteLine(myInt);
+            uint myUint = 9636963;
+            Console.WriteLine(myUint);
+
+            string value1 = "I control text";
+            string value2 = "23";
+            int intFromString = Int32.Parse(value2);
+            Console.WriteLine(value1);
+            Console.WriteLine(value2);
+            Console.WriteLine(intFromString);
         }
     }
 }
