7c95b33f03c166e350be4d57a519e4a2cc1c9e80
Drew <dreww@mortgage365.com>
2021-03-12 09:29:13 -0700

    Adds console read and input methods

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,17 +6,35 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            int num1 = 13;
-            int num2 = 5;
-            int sum = num1 + num2;
+            Console.Write("Enter a sting and press Enter:");
+            string readInput = Console.ReadLine();
+            Console.WriteLine("You have entered {0}", readInput);
 
-            double d1 = 3.5;
-            double d2 = 1.337;
-            double sumD = d1 + d2;
+            Console.Write("Enter a string and press Enter:");
+            int asciiValue = Console.Read();
+            Console.WriteLine("ASCII value is {0}", asciiValue);
+            Console.ReadKey();
+    
+           /* string myname = "Drew";
 
-            Console.WriteLine($"The sum of {num1} and {num2} is {sum}");
-            Console.WriteLine($"The sum of {d1} and {d2} is {sumD}");
+            string message = $"My name is {myname}";
+
+            string capsMessage = message.ToUpper();
+
+            Console.WriteLine(capsessage);
             Console.Read();
+
+              int num1 = 13;
+                int num2 = 5;
+                int sum = num1 + num2;
+
+                double d1 = 3.5;
+                double d2 = 1.337;
+                double sumD = d1 + d2;
+
+                Console.WriteLine($"The sum of {num1} and {num2} is {sum}");
+                Console.WriteLine($"The sum of {d1} and {d2} is {sumD}");
+                Console.Read(); */
         }
     }
 }
