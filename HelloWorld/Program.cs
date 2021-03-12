33974a13fc3962849e14dda81310b6d171badde9
Drew <dreww@mortgage365.com>
2021-03-12 08:07:38 -0700

    C# data types playground

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,8 +6,16 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello Drew!");
+            int num1 = 13;
+            int num2 = 5;
+            int sum = num1 + num2;
+
+            double d1 = 3.5;
+            double d2 = 1.337;
+            double sumD = d1 + d2;
+
+            Console.WriteLine($"The sum of {num1} and {num2} is {sum}");
+            Console.WriteLine($"The sum of {d1} and {d2} is {sumD}");
             Console.Read();
         }
     }
