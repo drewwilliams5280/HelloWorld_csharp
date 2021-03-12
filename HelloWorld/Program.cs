805984080810a067a16e2a7d6991f892dc9ad93a
Drew <dreww@mortgage365.com>
2021-03-12 13:32:09 -0700

    43

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,19 +4,18 @@ namespace HelloWorld
 {
     class Program
     {
-        const string friend1 = "Patrick";
-        const string friend2 = "Nolan";
-        const string friend3 = "Jack";
+  
         static void Main(string[] args)
         {
-            GreetFriend(friend1, friend2, friend3);
+            Console.WriteLine("Enter first number:");
+            string input1 = Console.ReadLine();
+            Console.WriteLine("Enter number to add to first number:");
+            string input2 = Console.ReadLine();
+            int total = Int32.Parse(input1) + Int32.Parse(input2);
+            Console.WriteLine(total);
+            Console.Read();
         }
         // access mmodifier (static) return type method name (parameter1, parameter2)
-        public static void GreetFriend(string friend1, string friend2, string friend3)
-        {
-            Console.WriteLine(friend1);
-            Console.WriteLine(friend2);
-            Console.WriteLine(friend3);
-        }
+       
     }
 }
