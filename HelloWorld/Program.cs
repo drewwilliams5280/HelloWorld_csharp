a6cd754b92e5835c474157ee5b454bf57d8ec457
Drew <dreww@mortgage365.com>
2021-03-15 10:49:06 -0600

    64

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,10 +6,19 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            for(int counter = 1; counter <= 20; counter += 2)
+            int lengthOfText = 0;
+            string wholeText = "";
+            do
             {
-                Console.WriteLine(counter);
-            }
+                Console.WriteLine("Please enter the name of a friend:");
+                string nameOfFriend = Console.ReadLine();
+                int currentLength = nameOfFriend.Length;
+                lengthOfText += currentLength;
+                wholeText += nameOfFriend;
+                Console.WriteLine(wholeText);
+      
+            } while (lengthOfText < 20);
+            Console.WriteLine("Thanks, that was enough");
             Console.Read();
         }
     }
