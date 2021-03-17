cce6d1e88e7e306c4befb9df3d5dbd19452de14e
Drew <dreww@mortgage365.com>
2021-03-17 11:39:19 -0600

    85 challenge

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Arrays
 {
@@ -6,24 +7,56 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            int[] nums = new int[10];
-
-            for(int i = 0; i < 10; i++)
+            dynamic firstResponse;
+            Console.WriteLine("Enter anything:");
+            string firstResponseString = Console.ReadLine();
+            if(int.TryParse(firstResponseString, out int firstResponseInt))
             {
-                nums[i] = i*i;
+                firstResponse = firstResponseInt;
             }
-
-            foreach(int num in nums)
+            else if(bool.TryParse(firstResponseString, out bool firstResponseBool))
             {
-                Console.WriteLine(num);
+                firstResponse = firstResponseBool;
             }
-
-            string[] friends = { "Nolan", "Patrick", "Victoria", "Keegan", "Colin" };
-
-            foreach(string friend in friends)
+            else
             {
-                Console.WriteLine($"Hi there, {friend}!");
-            } 
+                firstResponse = firstResponseString;
+            }
+            SecondStep(firstResponse);
+        }
+
+        static void SecondStep(dynamic firstResponse)
+        {
+            Console.WriteLine("Enter 1 for String");
+            Console.WriteLine("Enter 2 for Integer");
+            Console.WriteLine("Enter 3 for Boolean");
+            int intResponse;
+            string secondResponse = Console.ReadLine();
+            if(int.TryParse(secondResponse, out intResponse) && Enumerable.Range(1,3).Contains(intResponse))
+            {
+                FinalStep(firstResponse, intResponse);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+                SecondStep(firstResponse);
+            }
+        }
+
+        static void FinalStep(dynamic firstResponse, int intResponse)
+        {
+            if(intResponse == 1)
+            {
+                Console.WriteLine($"Your first input was {firstResponse} and you claim it is a String. That is: {firstResponse.GetType() == typeof(string)}");
+            }
+            else if(intResponse == 2)
+            {
+                Console.WriteLine($"Your first input was {firstResponse} and you claim it is an Integer. That is: {firstResponse.GetType() == typeof(int)}");
+            }
+            else
+            {
+                Console.WriteLine($"Your first input was {firstResponse} and you claim it is a Boolean. That is: {firstResponse.GetType() == typeof(bool)}");
+            }
         }
     }
 }
