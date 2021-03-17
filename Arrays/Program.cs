31499217667b26dfa8a8eccbe9cdcabdaa9784ef
Drew <dreww@mortgage365.com>
2021-03-17 10:26:26 -0600

    83

---
+++ Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = new int[5];
+
+            grades[0] = 97;
+            grades[1] = 67;
+            grades[2] = 77;
+            grades[3] = 73;
+            grades[4] = 89;
+
+            Console.WriteLine($"There are {grades.Length} grades total. They are:");
+
+            foreach (var grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
+
+            string[] newGrades = { "97", "87", "77", "67", "60" };
+
+            Console.WriteLine("String grades:");
+
+            foreach (string grade in newGrades)
+            {
+                Console.WriteLine(grade);
+            }
+        }
+    }
+}
