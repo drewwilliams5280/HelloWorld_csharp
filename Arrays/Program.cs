24a8d97356ea8840a148ade600f0237fd799c1d3
Drew <dreww@mortgage365.com>
2021-03-18 11:31:08 -0600

    91

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -7,33 +7,26 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            //declare jagged array
-            int[][] jaggedArray = new int[3][];
-
-            jaggedArray[0] = new int[5];
-            jaggedArray[1] = new int[3];
-            jaggedArray[2] = new int[2];
-
-            jaggedArray[0] = new int[] { 1, 2, 3, 4, 5 };
-            jaggedArray[1] = new int[] { 1, 2, 3 };
-            jaggedArray[2] = new int[] { 1, 2 };
-
-            //alternative way:
-            int[][] jaggedArray2 = new int[][]
+            int[] happinessArray = { 5, 6, 5, 7, 6 };
+            Console.WriteLine("Before the sun is shining, happiness levels are:");
+            foreach(int num in happinessArray)
             {
-                new int[] { 1, 2, 5, 7, 9 },
-                new int[] { 4, 6, 7 }
-            };
-
-            Console.WriteLine("The value in the middle of the first entry is {0}", jaggedArray2[0][2]);
-
-            for(int i = 0; i < jaggedArray2.Length; i++)
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("After the sun is shining, happiness levels are:");
+            SunIsShining(happinessArray);
+            foreach(int num in happinessArray)
             {
-                Console.WriteLine("Element {0}", i);
-                for(int j = 0; j < jaggedArray2[i].Length; j++)
-                {
-                    Console.WriteLine("Number: {0}", jaggedArray2[i][j]);
-                }
+                Console.WriteLine(num);
+            }
+            
+        }
+
+        static void SunIsShining(int[] happinessArray)
+        {
+            for(int i = 0; i < happinessArray.Length; i++)
+            {
+                happinessArray[i] += 2;
             }
         }
     }
