6faacaa2891fd6abfa0c38bb5cff45ddbb2437db
Drew <dreww@mortgage365.com>
2021-03-22 11:17:00 -0600

    post progress

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -6,7 +6,11 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Post post1 = new Post("Thanks for the birthday wishes", true, "Drew");
+            Console.WriteLine(post1.ToString());
+            ImagePost imagePost1 = new ImagePost("Check out my new shoes!", "Drew", "www.images.com/shoes", true);
+            Console.WriteLine(imagePost1.ToString());
+            Console.ReadLine();
         }
     }
 }
