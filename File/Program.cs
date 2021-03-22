eb07cb5a35b79bb97782278c0c4f96f0b82b847f
Drew <dreww@mortgage365.com>
2021-03-22 14:42:06 -0600

    115

---
+++ File/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace FileTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example 1 - reading text
+            string text = System.IO.File.ReadAllText(@"C:\Users\DrewW\source\docs\textFile.txt");
+
+            Console.WriteLine(text);
+            //read each line
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\DrewW\source\docs\textFile.txt");
+
+            foreach(string line in lines)
+            {
+                Console.WriteLine("\t" + line);
+            }
+
+            string[] writeLines = { "first line", "second line", "third line" };
+
+            File.WriteAllLines(@"C:\Users\DrewW\source\docs\textFile.txt", writeLines);
+
+            lines = System.IO.File.ReadAllLines(@"C:\Users\DrewW\source\docs\textFile.txt");
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\t\t" + line);
+            }
+        }
+    }
+}
