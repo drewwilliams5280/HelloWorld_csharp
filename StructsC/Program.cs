6b05904fba9ede1e23dba58a0b410a9494c3fc20
Drew <dreww@mortgage365.com>
2021-03-29 12:58:29 -0600

    basic struct 120

---
+++ StructsC/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StructsC
+{
+    struct Game
+    {
+        public string name;
+        public string developer;
+        public double rating;
+        public string releaseDate;
+
+        public void Display()
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(developer);
+            Console.WriteLine(rating);
+            Console.WriteLine(releaseDate);
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game1;
+            game1.name = "Pokemon Go";
+            game1.developer = "Niantic";
+            game1.rating = 3.5;
+            game1.releaseDate = "01.07.20";
+
+            game1.Display();
+        }
+    }
+}
