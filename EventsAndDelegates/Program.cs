29b63d9e74a452b78b6e92fee746e64d8569306a
Drew <dreww@mortgage365.com>
2021-03-30 14:35:13 -0600

    132

---
+++ EventsAndDelegates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EventsAndDelegates
+{
+    class Program
+    {
+
+        public delegate double PerformCalculation(double x, double y);
+
+        public static double Addition(double a, double b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            PerformCalculation getSum = Addition;
+            getSum(5.0, 5.0);
+        }
+    }
+}
