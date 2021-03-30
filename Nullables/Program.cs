cdce8501ea81867f6ae63bb066ba014f24a7bb10
Drew <dreww@mortgage365.com>
2021-03-30 13:41:27 -0600

    127

---
+++ Nullables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NullablesE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? num1 = null;
+            int? num2 = 1337;
+
+            double? num3 = new Double?();
+            double? num4 = 3.14;
+
+            bool? boolval = new bool?();
+
+            Console.WriteLine("Numbers: {0} {1} {2} {3} {4}", num1, num2, num3, num4, boolval);
+
+            double? num5 = null;
+
+        }
+    }
+}
