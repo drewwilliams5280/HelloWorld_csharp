1881794e7002e4e280069a0f202ab07b8a9de7f0
Drew <dreww@mortgage365.com>
2021-03-30 14:19:17 -0600

    130

--- Nullables/Program.cs
+++ Nullables/Program.cs
@@ -1,23 +1,16 @@
 ﻿using System;
 
-namespace NullablesE
+namespace AbstractClasses
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int? num1 = null;
-            int? num2 = 1337;
+            Cube myCube = new Cube(4.0);
+            myCube.GetInfo();
 
-            double? num3 = new Double?();
-            double? num4 = 3.14;
-
-            bool? boolval = new bool?();
-
-            Console.WriteLine("Numbers: {0} {1} {2} {3} {4}", num1, num2, num3, num4, boolval);
-
-            double? num5 = null;
-
+            Sphere mySphere = new Sphere(12.0);
+            mySphere.GetInfo();
         }
     }
 }
---
+++ Nullables/Sphere.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClasses
+{
+    class Sphere : Shape
+    {
+        public double Circumference { get; set; }
+
+        public Sphere(double circumference)
+        {
+            Name = "Sphere";
+            Circumference = circumference;
+        }
+
+        public double Radius()
+        {
+            return Circumference / 2;
+        }
+
+        public override double Volume()
+        {
+            return (4.0 / 3 * Math.PI * Math.Pow(Radius(), 3));
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"The Sphere has a circumference of {Circumference}");
+            Console.WriteLine($"The Sphere has a volume of {Volume()}");
+        }
+    }
+}
